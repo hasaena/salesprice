845711d487c036b5b608d72784059bfb1ff5020b
shhan826 <email>
2022-01-12 10:40:12 +0900

    zero input possible

--- docs/main.js
+++ docs/main.js
@@ -1,25 +1,39 @@
-let buyPrice = 0;
+let buyPrice = -1;
 function saveBuyPrice() {
-  buyPrice = Number(document.getElementById('input1').value);
+  buyPrice = document.getElementById('input1').value;
 }
 
-let deliveryFee = 0;
+let deliveryFee = -1;
 function saveDeliveryFee() {
-  deliveryFee = Number(document.getElementById('input2').value);
+  deliveryFee = document.getElementById('input2').value;
 }
 
-let weight = 0;
+let weight = -1;
 function saveWeight() {
-  weight = Number(document.getElementById('input3').value);
+  weight = document.getElementById('input3').value;
+}
+
+function checkNum(string) {
+  if (string === '0') {
+    return true;
+  } else if (Number(string) === 0) {
+    return false;
+  } else if (string === -1) {
+    return false;
+  }
+  return true
 }
 
 function calculatePrice() {
   let result = 0;
-  if (buyPrice === 0 || deliveryFee === 0 || weight === 0) {
-    alert('Please input only number except 0.');
+  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 20;
+  if (checkNum(buyPrice) === false || checkNum(deliveryFee) === false || checkNum(weight) === false) {
+    alert('Please input number.');
     result = 'NaN';
+  } else if (Number(buyPrice) < 0 || Number(deliveryFee) < 0 || Number(weight) < 0) {
+      alert('Please input positive number.');
+      result = 'NaN';
   } else {
-    let expectedLanding = ( buyPrice + deliveryFee + weight * 8.5 ) * 20;
     if (expectedLanding > 1800000) {
       result = Math.ceil(expectedLanding * 1.08 / 10000) * 10000;
     } else if (expectedLanding > 1500000) {
