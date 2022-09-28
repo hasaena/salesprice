722349d4e1808639dacdcf07a47cded0913b32f3
Sanghoon Han <shhan826@gmail.com>
2022-09-28 22:58:47 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -26,15 +26,15 @@ function checkNum(string) {
 
 function calculatePrice() {
   let result = 0;
-  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 20;
+  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 9.5 ) * 18;
   if (checkNum(buyPrice) === false || checkNum(deliveryFee) === false || checkNum(weight) === false) {
     alert('빈 칸에 숫자를 입력해주세요.');
     result = '오류';
   } else if (Number(buyPrice) < 0 || Number(deliveryFee) < 0 || Number(weight) < 0) {
     alert('0보다 큰 숫자를 입력해주세요.');
     result = '오류';
-  } else if (Number(buyPrice) < 10000) {
-    alert('10,000 KRW 미만 제품은 사장님에게 문의하세요.');
+  } else if (Number(buyPrice) < 8000) {
+    alert('8,000 KRW 미만 제품은 사장님에게 문의하세요.');
     result = '오류';
   } else {
     if (expectedLanding > 1800000) {
@@ -44,14 +44,13 @@ function calculatePrice() {
     } else if (expectedLanding > 1200000) {
       result = Math.ceil(expectedLanding * 1.11 / 10000) * 10000;
     } else if (expectedLanding > 900000) {
-      result = Math.ceil(expectedLanding * 1.13 / 10000) * 10000;
-    } else if (expectedLanding > 600000) {
       result = Math.ceil(expectedLanding * 1.15 / 10000) * 10000;
+    } else if (expectedLanding > 600000) {
+      result = Math.ceil(expectedLanding * 1.18 / 10000) * 10000;
     } else if (expectedLanding > 300000) {
-      result = Math.ceil(expectedLanding * 1.17 / 10000) * 10000;
+      result = Math.ceil(expectedLanding * 1.22 / 10000) * 10000;
     } else {
-      result = Math.ceil(expectedLanding * 1.19 / 10000) * 10000;
+      result = Math.ceil(expectedLanding * 1.25 / 10000) * 10000;
     }
   }
   document.getElementById('result').textContent = '판매가 : ' + result;
-}
