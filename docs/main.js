7ecce8e9ed81f7d7d6ee49df70465ec2680ccfd5
hasaena <122267051+hasaena@users.noreply.github.com>
2023-01-30 12:11:59 +0700

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -26,7 +26,7 @@ function checkNum(string) {
 
 function calculatePrice() {
   let result = 0;
-  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.8 ) * 19.39;
+  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 19.67;  /* 230130 환율 19.1의 3% 반영 */
   if (checkNum(buyPrice) === false || checkNum(deliveryFee) === false || checkNum(weight) === false) {
     alert('빈 칸에 숫자를 입력해주세요.');
     result = '오류';
