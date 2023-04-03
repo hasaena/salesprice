bda70d142b6b8fedd675846a7dbc733570259d82
hasaena <122267051+hasaena@users.noreply.github.com>
2023-04-03 14:09:30 +0700

    Update minmin.js

--- docs/minmin.js
+++ docs/minmin.js
@@ -26,10 +26,11 @@ function checkNum(string) {
 
 function calculatePrice() {
   let result = 0;
-  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 18.60;  
+  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 18.37;  
   /* 230130 환율 19.1의 3% 반영 */
   /* 230210 환율 18.65의 3% 반영 */
   /* 230228 환율 18.05의 3% 반영 */
+  /* 230403 환율 17.83의 3% 반영 */
   
   if (checkNum(buyPrice) === false || checkNum(deliveryFee) === false || checkNum(weight) === false) {
     alert('빈 칸에 숫자를 입력해주세요.');
