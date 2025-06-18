2be2192084929551bd3391401406c09d746be867
hasaena <122267051+hasaena@users.noreply.github.com>
2025-06-18 13:49:09 +0700

    Update minmin.js

--- docs/minmin.js
+++ docs/minmin.js
@@ -26,7 +26,7 @@ function checkNum(string) {
 
 function calculatePrice() {
   let result = 0;
-  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.5 ) * 20;  
+  let expectedLanding = ( Number(buyPrice) + Number(deliveryFee) + Number(weight) * 8.74 ) * 20;  
   /* 230130 환율 19.1의 3% 반영 */
   /* 230210 환율 18.65의 3% 반영 */
   /* 230228 환율 18.05의 3% 반영 */
